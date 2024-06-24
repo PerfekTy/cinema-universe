bb8f7cd14567fa61036d774f1070760617280708
Sebastian <sebastianfelenczak@gmail.com>
2024-06-24 19:34:46 +0200

    initial commit with shadcn and theme switching

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { ThemeSwitcher } from "@/components/theme-switcher";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ThemeSwitcher />
+    </main>
+  );
+}
